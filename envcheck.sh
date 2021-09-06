50dc8ac0bb8b5b3e3d8e1ecd6ee95d77bcb58fec
Wilson Javier Forero <wjforero@pop-os.localdomain>
2021-09-05 19:28:10 -0500

    Adding linux installation script

--- envcheck.sh
+++ envcheck.sh
@@ -1,12 +1,15 @@
 echo ------- Compilers
-python --version
+python3 --version
 java --version
 javac --version
+echo node
 node --version
 
 echo ------- Project managers
+pip --version
 mvn --version
-gradle--version
+gradle --version
+echo npm
 npm --version
 
 echo ------- Git
@@ -14,7 +17,7 @@ git --version
 
 echo ------- Running tests
 cd tests
-python testpy.py
+python3 testpy.py
 javac -source 11 -target 11 Hola.java
 java Hola
 node Hola.js
---
+++ install-linux.sh
@@ -0,0 +1,12 @@
+echo ----- InstallingPython
+sudo apt install python3-pip
+echo ----- Installing Java JDK 11
+sudo apt install openjdk-11-jre-headless
+sudo apt install openjdk-11-jdk-headless
+echo ----- Installing nodejs
+sudo apt install nodejs
+sudo apt install npm
+echo ----- Installing Maven and Gradle
+sudo apt install maven
+sudo apt install gradle
+
